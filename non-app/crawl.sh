a0576cc324f4aa7e30b0afcb8994a73bb5a5dc90
jouellnyc <jouellnyc@gmail.com>
2021-02-11 23:26:48 -0500

    ++NASDAQ

--- non-app/crawl.sh
+++ non-app/crawl.sh
@@ -1,9 +1,47 @@
 #!/bin/bash
 
-STOCKS=nasdaq.txt
+COUNTER=0
+SLEEP=62
 
-while read line; do
+while read stk; do echo == $stk ==;
 
-../lib/crawler.py -s WWW
 
-done < $STOCKS
+if [ -e /tmp/$stk ]; then
+
+        ls -l /tmp/$stk
+	echo "Already crawled $stk"
+
+else
+
+	OUT=$(./crawler.py $stk)
+	if echo $OUT | grep already; then
+	    touch /tmp/$stk
+	elif echo $OUT | grep -i 'no data'; then
+	    echo sleep $SLEEP
+	    sleep $SLEEP
+	    touch /tmp/$stk
+	    echo $stk >> remove.txt
+	elif echo $OUT | grep -i 'likely a data issue'; then
+	    touch /tmp/$stk
+	    echo $stk >> remove.txt
+	elif echo $OUT | grep -i 'limit'; then
+	    echo 'hit api limit'
+	    exit
+	else
+
+	    let COUNTER=COUNTER+1
+	    echo = $COUNTER =
+
+	    if [ $COUNTER -eq 400 ]; then
+		exit
+	    fi
+
+	    echo sleep $SLEEP
+	    sleep $SLEEP
+	fi
+
+
+fi
+
+
+done < sto.txt
