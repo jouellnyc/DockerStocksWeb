e85051f9e4cc7fcd68c794e291e7cb72df0770b4
jouellnyc <jouellnyc@gmail.com>
2021-04-25 23:34:01 -0400

    Refactor to Functions #1

--- lib/crawler.py
+++ lib/crawler.py
@@ -7,6 +7,7 @@ from pprint import pprint
 import datetime
 import time
 
+
 from alpha_vantage.fundamentaldata import FundamentalData
 from pymongo.errors import ServerSelectionTimeoutError
 from millify import millify
@@ -34,7 +35,7 @@ def cut(x):
 def add_str(string):
     return string + "Years"
 
-def stock_exists(stock):
+def get_stock_data(stock):
     try:
         data = mg.lookup_stock(stock)
     except ValueError:
@@ -42,76 +43,44 @@ def stock_exists(stock):
     else:
         return True, data
 
+class PassNoAV(Exception):
+    pass
 
-def main(stock, force, force_new):
+def stock_is_crawled_recently(stock_data, old_enough=None):
+    
+    old_enough = 3
+    date_crawled = stock_data ['DateCrawled']
+    difference = datetime.datetime.utcnow() - date_crawled
 
-    """
+    if difference.days >  old_enough:
+        return False
+    return True
 
-        force = False will not crawl if stock is in the DB at all
-        force = True  will try the date:
 
-            force_new  = True  will crawl no matter how old the data is
-            force_new  = False check's if it's X days old
+def Main(stock, force_new_all):
 
-    """
-
+    """ if force_new_all is True, crawl the doc """
+    """ otherwise check it's crawl date         """
+    
     debug = True
-
-    _, data = stock_exists(stock)
-
+    stock_data = get_stock_data(stock)
 
     if debug:
-        print("data:\n", data)
-        print("force force_new:", force, force_new)
+        print("data:\n", stock_data )
+     
+    if force_new_all:
+        print(f"Crawling and Indexing {stock} - due to force_new_all ")
+        CrawlStock(stock)
+    
+    print("...Checking Crawl Date")
+    if stock_is_crawled_recently:
+        print(f"Passing on {stock} - crawled recently")
+        raise PassNoAV
+    print(f"Crawling and Indexing {stock} due to out of date Crawl date")
+    CrawlStock(stock)
 
-    if data:
-
-        print(f"{stock} is already in Mongo")
-        if force is False:
-            print("...Passing due to force option")
-            return True
-        else:
-            print("...Trying due to force option")
-
-            try:
-
-                old_enough = 3
-                date_crawled = data['DateCrawled']
-                difference = datetime.datetime.utcnow() - date_crawled
-
-                if force_new is False:
-                    print("...Checking Crawl Date")
-                    if difference.days >  old_enough:
-                        print(f"Crawling and Indexing {stock} - Crawl date more than {old_enough} days")
-                        pass
-                    else:
-                        print(f"Passing on {stock} - crawled within {old_enough} days")
-                        return True
-                else:
-                    print(f"Crawling and Indexing {stock} - due to force_new")
-                    pass
-
-
-            except KeyError:
-
-                print(f"{stock} does not have crawl date")
-                if force_retry_blank:
-                    print(f"Crawling and Indexing {stock} - force_retry_blank is set")
-                    pass
-                else:
-                    print("...passing")
-                    return True
-
-            except TypeError:
-                print(f"{stock} does not have data or has incorrect data")
-                print("passing")
-                return False 
-
-    else:
-
-        print(f"{stock} was not already in Mongo -- continuing")
-        pass
-
+        
+def CrawlStock(stock):
     try:
 
         print(f"Connecting to Alpha Vantage for {stock}")
@@ -125,29 +94,12 @@ def main(stock, force, force_new):
 
         try:
             if ((df["totalRevenue"].iloc[0] == "0") or (df["totalRevenue"].iloc[0] == 0) or (df["totalRevenue"].iloc[-1] == "0") or (df["totalRevenue"].iloc[-1] == 0)):
-                print("0 Revenue -- Sending blank to Mongo")
-                return False
+                print("0 Revenue")
+                raise ValueError
         except IndexError as e:
             print(e)
             pass
 
-        """
-        else:
-            print(df["totalRevenue"].iloc[0])
-            print(type(df["totalRevenue"].iloc[0]))
-            print(len(df["totalRevenue"].iloc[0]))
-        """
-
-        """
-        df looks like this now:
-                  fiscalDateEnding  totalRevenue    netIncome
-        index
-        0           2019-12-31  161857000000  34343000000
-        1           2018-12-31  136819000000  30736000000
-        2           2017-12-31  110855000000  12662000000
-        3           2016-12-31   90272000000  19478000000
-        4           2015-12-31   74989000000  16348000000
-        """
 
         if debug:
             print("df:\n", df, "\n")
@@ -182,55 +134,6 @@ def main(stock, force, force_new):
         if debug:
             print("df:\n", df, "\n")
 
-        """
-        df looks like this now:
-               fiscalDateEnding  totalRevenue    netIncome  Years  Revenue_Growth  NetInc_Growth Years_From
-        index
-        0           2019-12-31  161857000000  34343000000   2019        0.212086       0.203908     4Years
-        1           2018-12-31  136819000000  30736000000   2018        0.221939       0.234225     3Years
-        2           2017-12-31  110855000000  12662000000   2017        0.215847      -0.119927     2Years
-        3           2016-12-31   90272000000  19478000000   2016        0.203803       0.191461     1Years
-        4           2015-12-31   74989000000  16348000000   2015        0.000000       0.000000     0Years
-
-        df.to_dict\('records'\) looks like this now:
-
-        [{'NetInc_Growth': 0.20390827677878742,
-          'Revenue_Growth': 0.21208612858558862,
-          'Years': 2019,
-          'Years_From': '4Years',
-          'fiscalDateEnding': '2019-12-31',
-          'netIncome': 34343000000,
-          'totalRevenue': 161857000000},
-         {'NetInc_Growth': 0.23422471699892022,
-          'Revenue_Growth': 0.22193925428985017,
-          'Years': 2018,
-          'Years_From': '3Years',
-          'fiscalDateEnding': '2018-12-31',
-          'netIncome': 30736000000,
-          'totalRevenue': 136819000000},
-         {'NetInc_Growth': -0.11992671079483375,
-          'Revenue_Growth': 0.21584681665796124,
-          'Years': 2017,
-          'Years_From': '2Years',
-          'fiscalDateEnding': '2017-12-31',
-          'netIncome': 12662000000,
-          'totalRevenue': 110855000000},
-         {'NetInc_Growth': 0.19146072914117918,
-          'Revenue_Growth': 0.20380322447292265,
-          'Years': 2016,
-          'Years_From': '1Years',
-          'fiscalDateEnding': '2016-12-31',
-          'netIncome': 19478000000,
-          'totalRevenue': 90272000000},
-         {'NetInc_Growth': 0.0,
-          'Revenue_Growth': 0.0,
-          'Years': 2015,
-          'Years_From': '0Years',
-          'fiscalDateEnding': '2015-12-31',
-          'netIncome': 16348000000,
-          'totalRevenue': 74989000000}]
-        """
-
         if debug:
             print("df.to_dict\('records'\):\n")
             pprint(df.to_dict('records'))
@@ -255,46 +158,6 @@ def main(stock, force, force_new):
                 "RevenueGrowth" : float(mk_pretty(year["Revenue_Growth"])),
             }
 
-        """
-        mongo_doc looks like this now:
-        {'Years': {'0Years': {'Date': '2015-12-31',
-                              'NetIncDenom': 'B',
-                              'NetIncGrowth': 0.0,
-                              'NetIncome': '16.35',
-                              'RevDenom': 'B',
-                              'Revenue': '74.99',
-                              'RevenueGrowth': 0.0},
-                   '1Years': {'Date': '2016-12-31',
-                              'NetIncDenom': 'B',
-                              'NetIncGrowth': 19.15,
-                              'NetIncome': '19.48',
-                              'RevDenom': 'B',
-                              'Revenue': '90.27',
-                              'RevenueGrowth': 20.38},
-                   '2Years': {'Date': '2017-12-31',
-                              'NetIncDenom': 'B',
-                              'NetIncGrowth': -11.99,
-                              'NetIncome': '12.66',
-                              'RevDenom': 'B',
-                              'Revenue': '110.86',
-                              'RevenueGrowth': 21.58},
-                   '3Years': {'Date': '2018-12-31',
-                              'NetIncDenom': 'B',
-                              'NetIncGrowth': 23.42,
-                              'NetIncome': '30.74',
-                              'RevDenom': 'B',
-                              'Revenue': '136.82',
-                              'RevenueGrowth': 22.19},
-                   '4Years': {'Date': '2019-12-31',
-                              'NetIncDenom': 'B',
-                              'NetIncGrowth': 20.39,
-                              'NetIncome': '34.34',
-                              'RevDenom': 'B',
-                              'Revenue': '161.86',
-                              'RevenueGrowth': 21.21}}}
-
-
-        """
         if debug:
             print("mongo_doc: \n")
             pprint(mongo_doc)
@@ -339,54 +202,6 @@ def main(stock, force, force_new):
         mongo_doc["PriceToBookRatio"] = float(millify(price2book, precision=2))
         mongo_doc["DateCrawled"] = datetime.datetime.utcnow()
 
-        """
-        mongo_doc now looks like this:
-
-        {'BookValue': 314.169,
-         'Currency': 'USD',
-         'Market_Cap': 1.28,
-         'Market_Cap_Denom': 'T',
-         'PriceToBookRatio': 6.02,
-         'PriceToSalesTTM': 7.61,
-         'RevTTM': 171.7,
-         'RevTTM_Denom': 'B',
-         'TrailingPE': 36.0,
-         'Years': {'0Years': {'Date': '2015-12-31',
-                              'NetIncDenom': 'B',
-                              'NetIncGrowth': 0.0,
-                              'NetIncome': '16.35',
-                              'RevDenom': 'B',
-                              'Revenue': '74.99',
-                              'RevenueGrowth': 0.0},
-                   '1Years': {'Date': '2016-12-31',
-                              'NetIncDenom': 'B',
-                              'NetIncGrowth': 19.15,
-                              'NetIncome': '19.48',
-                              'RevDenom': 'B',
-                              'Revenue': '90.27',
-                              'RevenueGrowth': 20.38},
-                   '2Years': {'Date': '2017-12-31',
-                              'NetIncDenom': 'B',
-                              'NetIncGrowth': -11.99,
-                              'NetIncome': '12.66',
-                              'RevDenom': 'B',
-                              'Revenue': '110.86',
-                              'RevenueGrowth': 21.58},
-                   '3Years': {'Date': '2018-12-31',
-                              'NetIncDenom': 'B',
-                              'NetIncGrowth': 23.42,
-                              'NetIncome': '30.74',
-                              'RevDenom': 'B',
-                              'Revenue': '136.82',
-                              'RevenueGrowth': 22.19},
-                   '4Years': {'Date': '2019-12-31',
-                              'NetIncDenom': 'B',
-                              'NetIncGrowth': 20.39,
-                              'NetIncome': '34.34',
-                              'RevDenom': 'B',
-                              'Revenue': '161.86',
-                              'RevenueGrowth': 21.21}}}
-        """
 
         if debug:
             print("mongo_doc:\n")
@@ -398,17 +213,16 @@ def main(stock, force, force_new):
     else:
         """ And now we are ready to send the Data to Mongo """
         print(f"OK, Sending data to Mongo for {stock}\n")
-        #print(mg.dbh.update_one({'Stock': stock}, {'$set' : mongo_doc }, upsert=True))
         print(mg.update_one_document({'Stock': stock}, {'$set' : mongo_doc }))
         print(f"OK, Updating {stock} as the lastest stock\n")
         print(mg.update_latest_stock(stock))
         return True
 
+
 if __name__ == "__main__" :
 
-    force = True
-    force_new = False 
-    force_retry_blank = True
+
+    force_new_all = True
     pause = 35
     debug = True
 
@@ -465,10 +279,10 @@ if __name__ == "__main__" :
     for stock in all_stocks:
         try:
             print(f"==== Trying {stock}")
-            if not main(stock, force=force, force_new=force_new):
-                print("Likely a Data Issue\nSending blank to Mongo")
-                mg.update_as_blank(stock)
-                sleepit(pause)
+            Main(stock, force_new_all=force_new_all)
+            sleepit(pause)
+        except PassNoAV:
+            pass
         except KeyError:
             print("Likely a Data Issue\nSending blank to Mongo")
             mg.update_as_blank(stock)
