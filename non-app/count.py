089cdab5fdb18cf8502bbdd0a0142c679e6175cb
jouellnyc <jouellnyc@gmail.com>
2021-04-26 17:33:07 -0400

    +++

--- non-app/count.py
+++ non-app/count.py
@@ -6,18 +6,21 @@ sys.path.append('../lib/')
 import mongodb
 mg = mongodb.MongoCli()
 
+for x in mg.dbh.find({'DateCrawled' : {"$exists": True } }): 
+    print(x['Stock'], x['DateCrawled'])
+
+"""
+
 for x in mg.dbh.find({'Stock' : {"$exists": True} }): 
     print(x['Stock'])
 
-"""
+
 for x in mg.dbh.find({'DateCrawled' : {"$exists": False } }): 
     print (x)
 
 
 for x in mg.dbh.find(): 
     print(x)
-    
-
 
 
 for x in mg.dbh.find({'Stock' : {"$exists": True} }): 
