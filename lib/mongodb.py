e4be99fe07fe6c8efb53abca9207b2902f8bec5a
jouellnyc <jouellnyc@gmail.com>
2021-04-25 20:53:05 -0400

    dump_all_stock

---
+++ lib/mongodb.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+
+""" mongodb.py - interface into MongoDB
+
+- This script requires the pymongo module.
+
+- This file is intended as a loadable module only.
+
+"""
+
+from pymongo import MongoClient
+
+class MongoCli:
+
+    def __init__(self):
+
+        self.database_name = "stocks"
+        self.collection_name = "data"
+        self.dbh = self.ConnectToMongo()
+
+    def ConnectToMongo(self):
+        """
+        Return a database_handle to the caller
+
+        Parameters
+        ----------
+        database_name : str
+            The Default db name
+        collection_name : str
+            The Default collection name
+
+        Returns
+        -------
+            collection_handle :  pymongo connect object
+        """
+
+        collection_name = self.collection_name
+        database_name = self.database_name
+
+        try:
+            client = MONGOCLIENTLINE
+            client.server_info()
+            database_handle = client[database_name]
+            collection_handle = database_handle[collection_name]
+            return collection_handle
+        except Exception:
+            raise
+
+    def lookup_stock(self, stock):
+        response = self.dbh.find_one({"Stock": stock}, {"_id": 0})
+        if response is None:
+            raise ValueError(f"{stock}  not found")
+        else:
+            return response
+
+    def dump_all_stocks_sorted_by_date(self):
+        """ Dump All Stocks with a Crawl Date, sort by that Date """
+
+        stocks = sorted( [( x['Stock'], x['DateCrawled'] ) for x in
+                                self.dbh.find( {'DateCrawled' : {"$exists": True }} )]
+                        , key= lambda one_date : one_date[1])
+        return [one_stock[0] for one_stock in stocks]
+
+    def dump_all_stocks(self):
+        """ Dump All Stocks, sorted Alphabetically """
+        stocks = sorted( [ x['Stock'] for x in self.dbh.find({'Stock' : {"$exists": True} }) ])
+        return stocks
+
+    def get_latest_stock(self):
+        """ Pull down the last stock that got crawled """
+        return self.dbh.find_one({'Stock':'last_good'},{'Name':1,'_id' : 0})['Name']
+
+    def update_latest_stock(self, stock):
+        """ Update DB with the last stock that got crawled """
+        return self.dbh.update_one({'Stock':'last_good'}, {'$set' : {'Name' : stock}} , upsert=True)
+
+    def update_as_blank(self, stock):
+        """ Replace existing Data with a blank if no Data Return from API """
+        return self.dbh.replace_one( {'Stock': stock},{'Stock': stock} )
+
+    def update_one_document(self,mongo_filter,  mongo_doc):
+        """
+        Update only one document in MongoDB, create it if it does not exist.
+        Parameters
+        ----------
+        mongo_filter
+            - hash to specify mongo restriction
+        mongo_doc
+            - key : value pairs making up the document
+
+        Returns
+        -------
+        Items
+            {MongoCli object}  of all the local posts in the free section
+        """
+        new_result = self.dbh.update_one(mongo_filter, mongo_doc, upsert=True)
+        return new_result
+
+    def insert_one_document(self, mongo_doc):
+        """
+        Insert  only one document in MongoDB; do not create it if it does not exist.
+
+        Parameters
+        ----------
+        mongo_doc
+            - key : value pairs making up the document
+
+        Returns
+        -------
+        Mongo Doc Id
+        """
+        new_result = self.dbh.insert_one(mongo_doc)
+        return new_result.inserted_id
+
+
+    def drop_db(self):
+        """
+        Drop all documents (testing/etc.)
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        new_result
+
+        Exceptions
+        ----------
+        Failure/Connection
+        """
+
+        new_result = self.dbh.drop()
+        return new_result
+
