b215f972814ab710927e53e32639571cf2a28569
jouellnyc <jouellnyc@gmail.com>
2021-04-26 17:31:55 -0400

    Init commit

---
+++ non-app/init_load.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('../lib/')
+
+import time
+import datetime
+
+from mongodb import MongoCli
+mg = MongoCli()
+
+if __name__ == "__main__" :
+
+    for file in [ 'nyse.txt', 'nasdaq.txt' ]:
+        mg = MongoCli()
+        for stock in open(f"../non-app/{file}",'r'):
+            stock=stock.strip()
+            if '^' in stock:
+                continue
+            date = datetime.datetime.utcnow()
+            if mg.insert_one_document({'Stock': stock, 'DateCrawled':date}):
+                print(f"{stock} loaded ok")
