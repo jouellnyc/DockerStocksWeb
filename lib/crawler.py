aad44ab31ed89a0f5f3695f115eb32df16f312e6
jouellnyc <jouellnyc@gmail.com>
2021-05-05 22:25:06 -0400

    +10 digit id

--- lib/crawler.py
+++ lib/crawler.py
@@ -57,7 +57,7 @@ def gen_crawlid():
     """ Generate a short id string for each crawler """
     return "".join(
         random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
-        for _ in range(8)
+        for _ in range(10)
     )
 
 
