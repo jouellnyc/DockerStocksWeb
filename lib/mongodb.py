b268115070a8af3a2a2dabd74f02520b2dfb350d
jouellnyc <jouellnyc@gmail.com>
2021-05-30 22:39:26 -0400

    Fix up update() and replace()

--- lib/mongodb.py
+++ lib/mongodb.py
@@ -15,6 +15,7 @@ from pymongo import MongoClient
 class StockDoesNotExist(Exception):
     pass
 
+
 class MongoCli:
     def __init__(self):
 
@@ -43,6 +44,9 @@ class MongoCli:
 
         try:
             client = MONGOCLIENTLINE
+                "mongodb+srv://stocku:pO1UvmV0wEsuUwm0@stockcluster.poxqf.mongodb.net/test?retryWrites=true&w=majority",
+                serverSelectionTimeoutMS=2000,
+            )
             client.server_info()
             database_handle = client[database_name]
             collection_handle = database_handle[collection_name]
@@ -70,6 +74,16 @@ class MongoCli:
         )
         return [one_stock[0] for one_stock in stocks]
 
+    def dump_recent_stocks(self):
+        """ Return all stocks split *after* the last crawled stock """
+        all_stocks = self.dump_all_stocks()
+        all_stocks_dict = {}
+        for i, stock in enumerate(all_stocks, 1):
+            all_stocks_dict[stock] = int(i)
+        latest_stock = self.get_latest_stock()
+        latest_stock_index = all_stocks_dict[latest_stock]
+        return all_stocks[latest_stock_index:]
+
     def dump_all_stocks(self):
         """ Dump All Stocks (All should have a CD), sorted Alphabetically """
         stocks = sorted(
@@ -87,7 +101,7 @@ class MongoCli:
         if result.acknowledged and result.deleted_count == 1:
             return True
         return False
-                
+
     def update_latest_stock(self, stock):
         """ Update DB with the last stock that got crawled """
         return self.dbh.update_one(
@@ -98,33 +112,24 @@ class MongoCli:
         """ Replace existing Data with a blank if no Data Return from API """
         return self.dbh.replace_one({"Stock": stock}, {"Stock": stock, "Error": msg})
 
-    def dump_recent_stocks(self):
-        """ Return all stocks split *after* the last crawled stock """
-        all_stocks = self.dump_all_stocks()
-        all_stocks_dict = {}
-        for i, stock in enumerate(all_stocks, 1):
-            all_stocks_dict[stock] = int(i)
-        latest_stock = self.get_latest_stock()
-        latest_stock_index = all_stocks_dict[latest_stock]
-        return all_stocks[latest_stock_index:]
 
     def update_one_document(self, mongo_filter, mongo_doc):
-        """ Update Fields on a doc, create if it does not exist. """
-        new_result = self.dbh.update_one(mongo_filter, mongo_doc, upsert=True)
+        """ Update a fields on a doc, create if it does not exist. """
+        new_result = self.dbh.update_one(mongo_filter, {"$set" : mongo_doc}, upsert=True) 
         return new_result
-
-    def insert_one_document(self, mongo_filter, mongo_doc):
+    
+    def insert_one_document(self, mongo_doc):
         """ Insert one document - do not create it if it does not exist. """
-        new_result = self.dbh.insert_one(mongo_filter, mongo_doc)
+        """ Will CREATE A DUP DOC WITH THE SAME STOCK SYMBOL!            """
+        new_result = self.dbh.insert_one(mongo_doc)
         return new_result.inserted_id
 
     def replace_one_document(self, mongo_filter, mongo_doc):
-        """ Replace one document in MongoDB entirely """
+        """ Replace one document in MongoDB entirely/%100 """
         new_result = self.dbh.replace_one(mongo_filter, mongo_doc, upsert=True)
         return new_result
-    
+
     def drop_db(self):
         """  Drop all documents (testing/etc.) """
         new_result = self.dbh.drop()
         return new_result
-    
