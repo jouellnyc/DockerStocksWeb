f5a838488ae2c5deab24fddd530e60a7fb3f682e
jouellnyc <jouellnyc@gmail.com>
2021-07-28 22:14:26 -0400

    Test Github Actions

--- non-app/count.py
+++ non-app/count.py
@@ -9,10 +9,10 @@ import mongodb
 mg = mongodb.MongoCli()
 
 def sa(how_to_sort):
+
     return sorted(
         [
             (x["Stock"], x["DateCrawled"], x["Crawled_By"])
-                #for x in mg.dbh.find({ "Crawled_By": {"$exists": True}, "Error" : {"$exists": False } })
                 for x in mg.dbh.find({ "Crawled_By": {"$exists": True}})
         ], key=how_to_sort,
     )
@@ -21,13 +21,25 @@ def by_date(stock):
     return stock[1]
 
 
-try:
-    for x in sa(how_to_sort=by_date):
+"""
+for x in mg.dbh.find({ "Crawled_By": {"$exists": True}}):
+    try:
+        print(x["Stock"],x['Error'],x['Crawled_By'],x['DateCrawled'],x['Success'])
+    except KeyError:
         print(x)
-except KeyError:
-    pass
+
+
+
+for x in sa(how_to_sort=by_date):
+    try:
+        print(x)
+    except KeyError:
+        pass
 
 """
+
 for x in mg.dbh.find(): 
-    print(x)
-"""
+    try:
+        print(x["Stock"],x['Error'],x['Success'])
+    except KeyError:
+        print(x)
--- non-app/count_err.py
+++ non-app/count_err.py
@@ -6,5 +6,6 @@ sys.path.append("../lib/")
 import mongodb
 mg = mongodb.MongoCli()
 
-for x in mg.dbh.find({'Error' : {"$exists": True } }): 
+#for x in mg.dbh.find({'Error' : {"$exists": True } }): 
+for x in mg.dbh.find( {'Error' : {"$ne": None }}): 
     print(x)
