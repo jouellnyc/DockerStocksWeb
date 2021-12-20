89b547c1460589b51e8cb5e5b4412a1ccda0387c
jouellnyc <jouellnyc@gmail.com>
2021-12-20 13:53:03 -0500

    Help Set AWS Env

--- non-app/set_aws_env.py
+++ non-app/set_aws_env.py
@@ -8,7 +8,7 @@ with open(file,'r') as fh:
             if 'key_id' in line:
                  ID=line.split('=')[1].replace(" ", "")
 
-with open('./aws_env','w') as fh:
+with open('../aws_env','w') as fh:
     fh.write(f"AWS_ACCESS_KEY_ID={ID}")
     fh.write(f"AWS_SECRET_ACCESS_KEY={KEY}")
     fh.write(f"AWS_DEFAULT_REGION=us-east-1")
--- non-app/set_aws_env.sh
+++ non-app/set_aws_env.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+cd non-app
 ./set_aws_env.py
+cd ..
 source aws_env
+set | grep AWS
