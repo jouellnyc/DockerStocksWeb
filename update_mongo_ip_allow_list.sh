f464c32ab3f49091dc8b365cb8b3f3880ae14ef1
jouellnyc <jouellnyc@gmail.com>
2022-04-10 15:33:04 -0400

    Fix update_mongo_ip_allow_list.sh SECRET VARS

--- update_mongo_ip_allow_list.sh
+++ update_mongo_ip_allow_list.sh
@@ -8,27 +8,25 @@
 
 PROD_API="v1.0"
 REGION="us-east-1"
+SECRET_ID="Prod-Stocks"
 
-export PRIVATE_KEY=$(aws secretsmanager get-secret-value --secret-id monogo_api_private_key --region REGION) 
-export PUBLIC_KEY=$(aws secretsmanager get-secret-value --secret-id monogo_api_publi_key --region REGION)
-export ALLOW_LIST_API_KEY_ID=$(aws secretsmanager get-secret-value --secret-id mongo_allow_list_api_key_id --region REGION)
-export ORG_ID=$(aws secretsmanager get-secret-value --secret-id mongo_org_id --region REGION)
-export GROUP_ID=$(aws secretsmanager get-secret-value --secret-id mongo_stocks_project_id --region REGION)
+export PRIVATE_KEY=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --query SecretString --region $REGION  --output text | jq  --raw-output '.monogo_api_private_key')
+export  PUBLIC_KEY=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --query SecretString --region $REGION  --output text | jq  --raw-output '.monogo_api_public_key')
+export      ORG_ID=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --query SecretString --region $REGION  --output text | jq  --raw-output '.mongo_org_id')
+export    GROUP_ID=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --query SecretString --region $REGION  --output text | jq  --raw-output '.mongo_stocks_project_id')
+export API_ALLOWS_LIST_KEY=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID --query SecretString --region $REGION  --output text | jq  --raw-output '.mongo_allow_list_api_key_id')
 export MY_IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+export POST_DATA=post_data.txt
+
+echo "[{\"ipAddress\":\"${MY_IP}\"}]" > $POST_DATA
 
 
 ### Group / Project updating the DB ACCESS Network list
-curl --user "${PUBLIC_KEY}:${PRIVATE_KEY}" --digest \
+curl -v --user "${PUBLIC_KEY}:${PRIVATE_KEY}" --digest \
      --header "Accept: application/json" \
      --header "Content-Type: application/json" \
      --request POST "https://cloud.mongodb.com/api/atlas/$PROD_API/groups/${GROUP_ID}/accessList?pretty=true" \
-     --data '
-       [
-         {
-           "ipAddress" : "${MY_IP}",
-           "comment" : "IP address for Application Server A"
-         }
-       ]'
+     --data @"${POST_DATA}"
 
 exit
 
@@ -56,3 +54,4 @@ curl --user "${PUBLIC_KEY}:${PRIVATE_KEY}" --digest \
        [{
           "ipAddress" : "77.54.32.11"
        }]'
+
