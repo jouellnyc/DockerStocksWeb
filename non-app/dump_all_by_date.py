cb28896cc41388cfe75f92fed8ccecbd88cda321
jouellnyc <jouellnyc@gmail.com>
2022-04-30 09:25:09 -0400

    +All

---
+++ non-app/dump_all_by_date.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import sys
+
+sys.path.append("../lib/")
+
+import mongodb
+
+mg = mongodb.MongoCli()
+
+
+def sa(how_to_sort):
+
+    return sorted(
+
+        [
+            (x["Stock"], x['Crawled_By'], x['Error'], x['DateCrawled'], x['Success']) for x in mg.dbh.find({ "Crawled_By": {"$exists": True}})
+        ], key=how_to_sort,
+    )
+
+def by_date(stock):
+    return stock[1]
+
+for x in sa(how_to_sort=by_date):
+    print(x)
