390c26dcba64bf2e7a9b5d3f4c916ea4f130bd35
jouellnyc <jouellnyc@gmail.com>
2022-05-27 16:44:56 -0400

    StreamLine Local Build and Push

--- build_aws_and_push.sh
+++ build_aws_and_push.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
+
+
 docker-compose -f docker-compose.local.yaml build
 docker tag docker_stocks_app:latest 631686326988.dkr.ecr.us-east-1.amazonaws.com/docker_stocks_app:latest
-docker push 631686326988.dkr.ecr.us-east-1.amazonaws.com/docker_stocks_app:latest || aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 631686326988.dkr.ecr.us-east-1.amazonaws.com
 
-docker tag docker_stocks_web:latest 631686326988.dkr.ecr.us-east-1.amazonaws.com/docker_stocks_web:latest
-docker push 631686326988.dkr.ecr.us-east-1.amazonaws.com/docker_stocks_web:latest || aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 631686326988.dkr.ecr.us-east-1.amazonaws.com
+for svc in app web; do 
 
+    PUSH="docker push 631686326988.dkr.ecr.us-east-1.amazonaws.com/docker_stocks_${svc}:latest"
+    if  ! $PUSH ; then
+        aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 631686326988.dkr.ecr.us-east-1.amazonaws.com 
+        $PUSH
+    fi
 
+done
