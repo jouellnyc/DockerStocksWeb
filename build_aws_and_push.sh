d42a56eef9dd66050124f06f6c12e84a08967fb2
jouellnyc <jouellnyc@gmail.com>
2022-06-05 00:15:57 -0400

    Align Slash Behaviour

--- build_aws_and_push.sh
+++ build_aws_and_push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-docker-compose -f docker-compose.local.yaml build
-docker tag docker_stocks_app:latest 631686326988.dkr.ecr.us-east-1.amazonaws.com/docker_stocks_app:latest
+#docker-compose -f docker-compose.local.yaml build
 
 for svc in app web; do 
 
+    docker tag docker_stocks_${svc}:latest 631686326988.dkr.ecr.us-east-1.amazonaws.com/docker_stocks_${svc}:latest
     PUSH="docker push 631686326988.dkr.ecr.us-east-1.amazonaws.com/docker_stocks_${svc}:latest"
     if  ! $PUSH ; then
         aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 631686326988.dkr.ecr.us-east-1.amazonaws.com 
