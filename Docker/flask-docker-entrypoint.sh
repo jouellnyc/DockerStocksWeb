1f6849963a65f162224e332cc9b7f06eccf6a9fe
jouellnyc <jouellnyc@gmail.com>
2022-06-07 13:42:28 -0400

    .env

--- Docker/flask-docker-entrypoint.sh
+++ Docker/flask-docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 WORKDIR="stocks"
 cd /$WORKDIR
-./lib/aws_secrets.py
+/$WORKDIR/lib/aws_secrets.py
 /usr/local/bin/gunicorn stock_flask:app  -c /$WORKDIR/external/gunicorn/gunicorn.conf.py
