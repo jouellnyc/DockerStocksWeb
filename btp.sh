08653ea9f87cec7f72fd769994a697eff72e892a
JohnO <jouellnyc@gmail.com>
2023-02-25 23:07:13 -0500

    Build Tag Psuh

--- btp.sh
+++ btp.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 source data/AWS.vars.txt
 #docker-compose  -f docker-compose.local.dev.yaml build 
 docker-compose  -f  docker-compose.AWS.hosted.MongoDb.bespoke.yaml  build 
 docker tag u2f_app:latest 554829083981.dkr.ecr.us-east-1.amazonaws.com/u2f_app:latest
+docker tag u2f_web:latest 554829083981.dkr.ecr.us-east-1.amazonaws.com/u2f_web:latest
 docker push 554829083981.dkr.ecr.us-east-1.amazonaws.com/u2f_app:latest
+docker push 554829083981.dkr.ecr.us-east-1.amazonaws.com/u2f_web:latest
 
