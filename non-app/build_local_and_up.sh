1a6075a0b939802c4a028d869edf15efd8c668b0
JohnO <jouellnyc@gmail.com>
2023-02-27 22:43:45 -0500

    One Dockerfile

--- non-app/build_local_and_up.sh
+++ non-app/build_local_and_up.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-docker-compose  -f docker-compose.Local.web.app.db.yaml build && docker-compose  -f docker-compose.Local.web.app.db.yaml up
 
+if grep MODE init.yaml | grep AWS; then
+
+    echo "Oops you are set to AWS"
+
+else
+
+    docker-compose  -f docker-compose.Local.web.app.db.yaml build && docker-compose  -f docker-compose.Local.web.app.db.yaml up
+
+fi
