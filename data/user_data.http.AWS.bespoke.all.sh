be9fcede0e6b3a495040b379f8e4f35676087081
JohnO <jouellnyc@gmail.com>
2023-02-28 10:45:30 -0500

    One Dockerfile

--- data/user_data.http.AWS.bespoke.all.sh
+++ data/user_data.http.AWS.bespoke.all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
 
-
 ### Update Software and utils
 yum update -y
 yum -y install git
@@ -13,12 +12,12 @@ yum -y install jq
 
 amazon-linux-extras install docker
 
+
 ### Install aws
 cd /tmp
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-/usr/local/bin/aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 631686326988.dkr.ecr.us-east-1.amazonaws.com
 
 ### Install Docker
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
@@ -32,27 +31,27 @@ chkconfig docker on
 service awslogsd start
 chkconfig awslogsd on
 
-### Start Build
+### Setup to Pull Images 
 GITHUB_REPO="https://github.com/jouellnyc/DockerStocksWeb.git"
 GIT_DIR="/gitrepos/"
 mkdir -p $GIT_DIR
 cd $GIT_DIR
 
-#*Vars
 git clone $GITHUB_REPO
 cd DockerStocksWeb
 source data/AWS.vars.txt
+/usr/local/bin/aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ECR_REPO
 
-#*Vars
+# Pull and Tag Images
 docker pull       $AWS_ECR_REPO/$SHORT_WEB_IMAGE
 docker image tag  $AWS_ECR_REPO/$SHORT_WEB_IMAGE $SHORT_WEB_IMAGE
 
-#*Vars
+# Pull and Tag Images
 docker pull       $AWS_ECR_REPO/$SHORT_APP_IMAGE
 docker image tag  $AWS_ECR_REPO/$SHORT_APP_IMAGE $SHORT_APP_IMAGE
 
-DOCKER_COMPOSE_FILE="docker-compose.AWS.hosted.MongoDb.bespoke.yaml"
-#cd lib; ./get_client_secrets_json.py && cd ..; 
+# Start in all up if we can update the Mongo IP ACL
+DOCKER_COMPOSE_FILE="docker-compose.AWS.web.app.Hosted.mongo.yaml"
 ./update_mongo_ip_allow_list.sh && docker-compose -f $DOCKER_COMPOSE_FILE up -d
 
 ### End
